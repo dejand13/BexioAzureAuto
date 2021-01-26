2cf968a0b5e0e297136b86adc00b3dedc7500bd0
Dejan's MacBook Pro <>
2021-01-26 18:25:17 +0100

    Add Contact test

--- src/test/java/com/bexio/manageContacts/AddContactTest.java
+++ src/test/java/com/bexio/manageContacts/AddContactTest.java
@@ -13,8 +13,8 @@ import java.io.IOException;
 
 import static io.appium.java_client.touch.offset.ElementOption.element;
 
-public class ManageContactsAddContactTest extends LoginTest {
-    public static Logger log = LogManager.getLogger(ManageContactsAddContactTest.class.getName());
+public class AddContactTest extends LoginTest {
+    public static Logger log = LogManager.getLogger(AddContactTest.class.getName());
 
     @Test
     public void manageContacts() throws IOException, InterruptedException {
@@ -26,50 +26,61 @@ public class ManageContactsAddContactTest extends LoginTest {
         log.info("Tapping on add new contact button");
         Thread.sleep(2000L);
 
+        TouchAction touch = new TouchAction(driver);
+
+//        String executionStore = prop.getProperty("store");
+        String executionStore = ("#{store}#");
+        if (executionStore.equalsIgnoreCase("android")) {
+            addContact.newPerson.click();
+        } else {
+            AddContactTest addNewPerson = new AddContactTest();
+            addNewPerson.newPersonCompanyCoordinates(0.65, 0.45);
+        }
+//        touch.tap(PointOption.point(300,420)).perform();
+
+        addContact.lastName.sendKeys("Last name");
+        addContact.firstName.sendKeys("First name");
+        Thread.sleep(2000L);
+        addContact.formOfAddress.click();
+        addContact.formOfAddressMr.click();
+        Thread.sleep(2000L);
+//        addContact.title.click();
+//        addContact.titleDr.click();
+        addContact.languageOfCorrespondence.click();
+        addContact.languageOfCorrItalian.click();
+
+        addContact.email.sendKeys("ddoneski@melontech.com");
+        addContact.email2.sendKeys("doneski.dejan@gmail.com");
+
+        addContact.phone.sendKeys("00358 72 281 1200");
+        addContact.phone2.sendKeys("00358 17 281 1210");
+        addContact.mobile.sendKeys("00358 66 321 9843");
+
+        addContact.website.sendKeys("www.google.com");
+        addContact.address.sendKeys("Torikatu");
+        addContact.postcode.sendKeys("70110");
+        addContact.city.sendKeys("Kuopio");
+        addContact.country.click();
+        Thread.sleep(2000L);
+        newPersonCompanyCoordinates(0.7, 0.001);
+        addContact.countryFinland.click();
+        addContact.remarks.sendKeys("Add some remarks");
+        addContact.category.click();
+//        addContact.categoryQA.click();
+//        addContact.checkboxSaveButton.click();
+//        addContact.sectors.click();
+//        addContact.checkboxSaveButton.click();
+        addContact.addContactSaveButton.click();
+        addContact.backButtonContactDetailView.click();
+        addContact.homeButton.click();
+    }
+    public void newPersonCompanyCoordinates(double widthPercentage, double heightPercentage) {
         int widthOfScreen = driver.manage().window().getSize().getWidth();
         int heightOfScreen = driver.manage().window().getSize().getHeight();
-
-        int x = (int) (widthOfScreen * 0.65);
-        int y = (int) (heightOfScreen * 0.45);
+        int x = (int) (widthOfScreen * widthPercentage);
+        int y = (int) (heightOfScreen * heightPercentage);
 
         TouchAction touch = new TouchAction(driver);
         touch.tap(PointOption.point(x,y)).perform();
-//        touch.tap(PointOption.point(300,420)).perform();
-//        addContact.newPerson.click();
-
-//        addContact.lastName.sendKeys("Doneski");
-//        addContact.firstName.sendKeys("Dejan");
-        Thread.sleep(2000L);
-        addContact.formOfAddress.click();
-        //addContact.formOfAddressMr.click();
-        Thread.sleep(2000L);
-        addContact.title.click();
-//        addContact.titleDr.click();
-//        addContact.languageOfCorrespondence.click();
-//        addContact.languageOfCorrItalian.click();
-//
-//        addContact.email.sendKeys("ddoneski@melontech.com");
-//        addContact.email2.sendKeys("doneski.dejan@gmail.com");
-//        touch.longPress(LongPressOptions.longPressOptions().withElement(element(addContact.email2)).withDuration(Duration.ofSeconds(2))).moveTo(element(addContact.country)).release().perform();
-//        addContact.phone.sendKeys("072/588-848");
-//        addContact.phone2.sendKeys("046/271-431");
-//        addContact.mobile.sendKeys("071/663-219");
-//        addContact.website.sendKeys("www.google.com");
-//        addContact.address.sendKeys("8th March");
-//        addContact.postcode.sendKeys("6000");
-        addContact.city.sendKeys("Ohrid");
-        //addContact.country.click();
-        Thread.sleep(2000L);
-        //touch.longPress(LongPressOptions.longPressOptions().withElement(element(addContact.countryBulgaria)).withDuration(Duration.ofSeconds(2))).moveTo(element(addContact.countryFinland)).release().perform();
-        //addContact.countryFinland.click();
-        addContact.remarks.sendKeys("Add some remarks");
-        addContact.category.click();
-        //touch.tap(TapOptions.tapOptions().withElement(ElementOption.element(addContact.categoryQA))).release().perform();
-        addContact.categoryQA.click();
-        addContact.checkboxSaveButton.click();
-        //touch.longPress(LongPressOptions.longPressOptions().withElement(element(addContact.lastName)).withDuration(Duration.ofSeconds(2))).moveTo(element(addContact.sectors)).release().perform();
-        addContact.sectors.click();
-        touch.tap(TapOptions.tapOptions().withElement(ElementOption.element(addContact.sector1B))).release().perform();
-        addContact.checkboxSaveButton.click();
     }
 }
